a21f29e0ff53f8dc045b6a99027d23801707663f
sergijolenuk50 <sergijolenuk50@gmail.com>
2025-01-21 23:48:34 +0200

    create

---
+++ tinderApi/users/admin.py
@@ -0,0 +1,14 @@
+# from django.contrib import admin
+
+# Register your models here.
+# from django.contrib import admin  
+# from .models import CustomUser  
+# from django.contrib.auth.admin import UserAdmin  
+
+# class CustomUserAdmin(UserAdmin):  
+#     model = CustomUser  
+#     add_fieldsets = UserAdmin.add_fieldsets + (  
+#         (None, {'fields': ('phone_number',)}),  # Додаткові поля  
+#     )  
+    
+# admin.site.register(CustomUser, CustomUserAdmin)  
